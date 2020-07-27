3d08c1b046bb35c404e37e6e6eb0a181c0cb31be
JakeVestal <jmvestal@gmail.com>
2020-07-27 16:33:30 -0400

    passes R CMD Check

--- R/FinancieR.R
+++ R/FinancieR.R
@@ -1,8 +1,17 @@
-#' @keywords internal
-"_PACKAGE"
+#' \code{FinancieR} package
+#'
+#' Useful financial functions
+#' 
+#' @docType package
+#' @name FinancieR
+#' 
+NULL
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "from", "to"))
