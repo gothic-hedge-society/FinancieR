0dfe8583471a9be481691d2d01dfcd931cb47d83
sweetClyde <jmvestal@gmail.com>
2020-07-27 14:20:14 -0400

    initial commit dev branch

---
+++ tests/testthat/test-calculate_returns.R
@@ -0,0 +1,62 @@
+context("Accuracy")
+
+# Do a few checks on returns that were calculated by spreadsheet.
+
+test_that(
+  paste0(
+    "calculate_returns() correctly handles Texas Instruments' Hurricane ",
+    "Sandy missing dividend for all returns_method options."
+  ),
+  expect_equivalent(
+    c("ln", "log2", "log10", "pct_diff", "multiple") %>%
+      vapply(
+        function(rtn_method){
+          zoo::coredata(
+            calculate_returns(
+              assets         = stock_data["TXN"],
+              date_range_xts = "2012-10",
+              returns_method = rtn_method
+            )["2012-10-31"]
+          )
+        },
+        numeric(1)
+      ) %>%
+      round(digits = 5),
+    round(
+      c(
+        "ln"        = -0.021671592641372,
+        "log2"      = -0.0312654992318732,
+        "log10"     = -0.00941185309820299,
+        "pct_diff"  = -0.0214384508990318,
+        "multiple"  =  0.978561549100968
+      ),
+      digits = 5
+    )
+  )
+)
+
+test_that(
+  "calculate_daily_returns() correctly handles Apple's 7-to-1 split in 2014.",
+  expect_equivalent(
+    c("ln", "log2", "log10", "pct_diff", "multiple") %>%
+      vapply(
+        function(rtn_method){
+          calculate_returns(
+            assets         = stock_data["AAPL"],
+            date_range_xts = "2014-06",
+            returns_method = rtn_method 
+          )["2014-06-09"] 
+        },
+        numeric(1)
+      ) %>%
+      round(digits = 5),
+    c(
+      "ln"        = 0.0158746908251272,
+      "log2"      = 0.0229023377290565,
+      "log10"     = 0.0068942906272729,
+      "pct_diff"  = 0.0160013631364528,
+      "multiple"  = 1.01600136313645
+    ) %>%
+      round(digits = 5)
+  )
+)
