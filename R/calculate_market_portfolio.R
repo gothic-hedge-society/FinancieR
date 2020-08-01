1e13a310de140ea390d0411a996af9c84a852658
JakeVestal <jmvestal@gmail.com>
2020-08-01 11:11:48 -0400

    included `prices` in `calculate_market_portfolio`() output, added documentation

--- R/calculate_market_portfolio.R
+++ R/calculate_market_portfolio.R
@@ -42,6 +42,50 @@
 #'   assets under management for which a market portfolio is to be calculated on
 #'   a shares basis. See "Returns" section for more info.
 #'    
+#' @details 
+#' 
+#'   All arguments which are percentages (\emph{exp_rtn}, \emph{exp_vol}, and
+#'   \emph{rfr}) must be supplied in decimal form; i.e., to specify "12%", use
+#'   0.12, not 12.
+#'   
+#'   It should go without saying, but make sure that every asset is represented
+#'   in the three inputs: names of \emph{exp_rtn}, \emph{exp_vol}, and the row &
+#'   colnames of \emph{exp_cor}.
+#' 
+#'   This function works by finding the Sharpe-optimum equal-weighted portfolio
+#'   that can be created using the assets passed in. Using that portfolio as a
+#'   starting point, the function finds the assets A and B such that
+#'   reallocating ("selling") a small amount (\emph{step}) of A into asset B
+#'   ("buying") results in a portfolio whose Sharpe is greater than all other
+#'   possible assets A and B. The portfolio variable is updated.
+#'   
+#'   This process is repeated with smaller and smaller \emph{step} sizes. When
+#'   it is not possible to reallocate \emph{step} amount of any asset A into any
+#'   other asset B so as to create a portfolio having a better Sharpe, the
+#'   Market Portfolio has been reached and the function returns the value.
+#'   
+#'   \strong{Weights} mode:
+#'   If either \emph{prices} or \emph{portfolio_aum} is unspecified in the call
+#'   to \emph{calculate_market_portfolio}(), then the function operates in
+#'   "\emph{Weights Mode}" and will attempt to answer the question: "What is the
+#'   the optimum fractional, real-valued weight of each asset in the
+#'   Sharpe-optimal MP?" Because the Sharpe ratio and the optimum weights are
+#'   real numbers, this operation can be computed with arbitrary precision, to
+#'   infinte decimal places. \emph{calculate_market_portfolio}() was written to
+#'   be clearly understood and run reasonably fast, and testing indicates that
+#'   the weights are reliable to within +/- 1% (i.e., "0.18" to "0.20" for a
+#'   reported weight of "0.19") of the optimal value. As such, weights reported
+#'   by \emph{weights mode} should be considered somewhat approximate.
+#' 
+#'   \strong{Shares} mode: 
+#'   A much different situation arises when we ask the question: "Given $5MM in
+#'   capital, what's the optimum portfolio that can be built from a set of
+#'   assets trading at given prices?" Note that the formulation of this question
+#'   requires both \emph{prices} and \emph{portfolio_aum} to be provided.
+#'   Because shares can only be bought in integer values, and because the sum of
+#'   the capital allocated to each asset (including "cash") must equal
+#'   \emph{portfolio_aum}, the number of values available for the \emph{shares}
+#'   vector is countably finite and it is possible to find an exact solution.
 #' 
 #' @return 
 #'   If \strong{weights basis} (\emph{prices} and \emph{portfolio_aum} 
@@ -64,37 +108,15 @@
 #'   assuming that  \emph{portfolio_aum} is available for investing and that the
 #'   price of each asset is given by \emph{prices}. Returns a list describing
 #'   the market portfolio (MP) which contains the for elements described above,
-#'   plus two more:
+#'   plus three more:
 #'   
 #'   \describe{
 #'     \item{shares:}{Named numeric vector giving the number of shares of each
 #'     asset calculated for the market portfolio}
-#'     \item{cash:}{Numeric. Leftover cash not invested in assets.}
+#'     \item{prices:}{Named umeric vector giving the prices used by 
+#'     calculate_market_portfolio() to determine the MP.}
+#'     \item{cash:}{Numeric, length 1. Leftover cash not invested in assets.}
 #'   }
-#'   
-#'   
-#'   
-#' @details 
-#' 
-#'   All arguments which are percentages (\emph{exp_rtn}, \emph{exp_vol}, and
-#'   \emph{rfr}) must be supplied in decimal form; i.e., to specify "12%", use
-#'   0.12, not 12.
-#'   
-#'   It should go without saying, but make sure that every asset is represented
-#'   in the three inputs: names of \emph{exp_rtn}, \emph{exp_vol}, and the row &
-#'   colnames of \emph{exp_cor}.
-#' 
-#'   This function works by finding the Sharpe-optimum equal-weighted portfolio
-#'   that can be created using the assets passed in. Using that portfolio as a
-#'   starting point, the function finds the assets A and B such that
-#'   reallocating ("selling") a small amount (\emph{step}) of A into asset B
-#'   ("buying") results in a portfolio whose Sharpe is greater than all other
-#'   possible assets A and B. The portfolio variable is updated.
-#'   
-#'   This process is repeated with smaller and smaller \emph{step} sizes. When
-#'   it is not possible to reallocate \emph{step} amount of any asset A into any
-#'   other asset B so as to create a portfolio having a better Sharpe, the
-#'   Market Portfolio has been reached and the function returns the value.
 #'   
 #' @example inst/examples/calculate_market_portfolio_ex.R
 #' 
@@ -449,11 +471,15 @@ calculate_market_portfolio <- function(
   
   mp_shares <- list(
     "sharpe"        = realized_sharpe,
-    "shares"        = realized_shares[-length(shares)],
+    "shares"        = realized_shares[-length(shares)] %>% {
+      storage.mode(.) <- "integer"
+      .
+    },
     "cash"          = as.numeric(prices[length(prices)]),
     "weights"       = realized_weights[-length(weights)],
     "ex_return"     = realized_exp_rtn,
-    "ex_volatility" = realized_exp_vol
+    "ex_volatility" = realized_exp_vol,
+    "prices"        = prices[-length(prices)]
   )
   
   mp_shares
