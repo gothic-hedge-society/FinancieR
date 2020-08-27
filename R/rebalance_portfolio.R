6666684f66d72db582ccccc18c7c4c860707e7af
JakeVestal <jmvestal@gmail.com>
2020-08-27 10:56:08 -0400

    skelly

---
+++ R/rebalance_portfolio.R
@@ -0,0 +1,14 @@
+#' Rebalance an Existing Portfolio
+#' 
+#' Given an existing portfolio and a set of expected returns, volatilities, and
+#' 
+#' 
+#' @inheritParams calculate_market_portfolio
+#'   
+#' @example inst/examples/rebalance_portfolio_ex.R
+#'   
+#' @export
+#' 
+rebalance_portfolio <- function(portfolio, exp_rtn, exp_vol, exp_cor, prices){
+  
+}
