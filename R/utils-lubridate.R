ecff381cc984d85d687190bed8d6e4fd9c203eca
JakeVestal <jmvestal@gmail.com>
2021-07-21 12:56:09 -0400

    checkpoint

---
+++ R/utils-lubridate.R
@@ -0,0 +1,23 @@
+#' Lubridate's `mplus`
+#'
+#' See \code{lubridate::\link[lubridate:mplus]{\%m\+\%}} for details.
+#'
+#' @name %m+%
+#' @rdname mplus
+#' @keywords internal
+#' @export
+#' @importFrom lubridate %m+%
+#' @usage lhs \%m\+\% rhs
+NULL
+
+#' Lubridate's `mplus`
+#'
+#' See \code{lubridate::\link[lubridate:mplus]{\%m\+\%}} for details.
+#'
+#' @name %m-%
+#' @rdname m-
+#' @keywords internal
+#' @export
+#' @importFrom lubridate %m-%
+#' @usage lhs \%m\-\% rhs
+NULL
